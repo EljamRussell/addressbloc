a835a5d097db26176b6cac0c2155ab87b395a89d
Eljam Russell <eljamrussell@gmail.com>
2017-07-10 21:41:48 -0400

    assignment 4 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,23 +1,40 @@
 require_relative 'entry'
 
-   class AddressBook
-     attr_reader :entries
+class AddressBook
+  attr_reader :entries
 
-     def initialize
-       @entries = []
-     end
+  def initialize
+    @entries = []
+  end
 
-   def add_entry(name, phone_number, email)
 
-     index = 0
+
+  def add_entry(name, phone_number, email)
+    index = 0
      entries.each do |entry|
-
        if name < entry.name
          break
        end
        index+= 1
      end
-
      entries.insert(index, Entry.new(name, phone_number, email))
-   end
- end
+  end
+
+
+
+
+
+  def remove_entry(name, phone_number, email)
+
+      delete_index = 0
+      entries.each do |remove_entry|
+
+        if name == remove_entry.name
+          break
+        end
+        delete_index+= 1
+      end
+
+      entries.delete_at(delete_index)
+    end
+end
