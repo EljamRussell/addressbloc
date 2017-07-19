a7fdecaf7e900fe2218c2a9e99617b2d29667e2a
Eljam Russell <eljamrussell@gmail.com>
2017-07-19 10:13:10 -0400

    checkpoint 7 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,3 +1,4 @@
+#  load the library using the relative address
 require_relative 'entry'
 require "csv"
 
@@ -27,5 +28,35 @@ class AddressBook
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
+  end
+
+  # Search AddressBook for a specific entry by name
+  # use var lower to save the index of the leftmost item in array
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    # use while loop while lower index less/equal upper index
+    while lower <= upper
+      # find middle index by sum of upper+lower / 2
+      # ruby truncates decimals, save in mid_name
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      # compare search name to mid_name using == (case sensitive)
+      # search name =  mid name....return
+      # search name < mid name ....set upper to mid-1
+      # search name > mid name ....set lower to mid+1
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    # if no match found...return
+    return nil
   end
 end
